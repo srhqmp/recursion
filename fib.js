ad752a7080715a5c49c0c71fc9ae201a529f2668
srhqmp <ballanosarah@gmail.com>
2024-09-26 13:18:11 +0800

    feat: implement fibs

---
+++ fib.js
@@ -0,0 +1,11 @@
+const fibs = (n) => {
+  const arr = [0, 1];
+  for (let i = 2; i < n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr;
+};
+
+console.log(fibs(8));
+
+const fibsRec = (n) => {};
