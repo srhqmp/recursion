3e2552d1b8175de9ce80a7ec19752cf6d4f926d2
srhqmp <ballanosarah@gmail.com>
2024-09-26 14:21:27 +0800

    feat: fibs rec

--- fib.js
+++ fib.js
@@ -6,6 +6,18 @@ const fibs = (n) => {
   return arr;
 };
 
-console.log(fibs(8));
+const fibsRec = (n) => {
+  console.log("This was printed recursively");
+  //   setup base cases
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
 
-const fibsRec = (n) => {};
+  //   build the array recursively
+  const arr = fibsRec(n - 1);
+  const nextFib = arr[arr.length - 1] + arr[arr.length - 2];
+  arr.push(nextFib);
+  return arr;
+};
+
+console.log(fibsRec(8));
